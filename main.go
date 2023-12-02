8870e0b935f77469ee2ee937ac67d183bcd5d3f1
Jan Romaniszyn <jan.romaniszyn@gmail.com>
2023-12-02 23:54:45 +0100

    cleanup

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,14 +11,6 @@ import (
 
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-var (
-	activeSessions = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "jellyfin_active_sessions_count",
-		Help: "Number of active sessions in Jellyfin",
-	})
 )
 
 var (
@@ -32,44 +23,31 @@ var (
 )
 
 func getSessions() (int, error) {
-	// Construct the URL with provided address and API key
 	url := jellyfinAddress + "/Sessions?api_key=" + apiKey
-
 	resp, err := http.Get(url)
 	if err != nil {
 		return 0, err
 	}
 	defer resp.Body.Close()
-
 	log.Printf("API request to %s completed with status code: %d", jellyfinAddress, resp.StatusCode)
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return 0, err
 	}
-
 	var jsonData interface{}
 	err = json.Unmarshal(body, &jsonData)
 	if err != nil {
 		return 0, err
 	}
-
-	// Use jsonpath to extract the NowPlayingItem count
 	result, err := jsonpath.Get("$[*].NowPlayingItem", jsonData)
 	if err != nil {
 		return 0, err
 	}
-
-	// Type assertion to extract the integer value
 	count := len(result.([]interface{}))
 	return count, nil
 }
 
 func main() {
-	flag.StringVar(&jellyfinAddress, "address", "", "Jellyfin instance address")
-	flag.StringVar(&apiKey, "apikey", "", "API key for Jellyfin")
-	flag.Parse()
-
 	jellyfinAddress = os.Getenv("JELLYFIN_ADDRESS")
 	apiKey = os.Getenv("API_KEY")
 	pollingIntervalStr := os.Getenv("POLLING_INTERVAL")
@@ -93,25 +71,7 @@ func main() {
 			c.String(http.StatusInternalServerError, "Error getting sessions count")
 			return
 		}
-		activeSessions.Set(float64(count))
 		c.String(http.StatusOK, "jellyfin_active_sessions_count %d", count)
 	})
-
-	go func() {
-		log.Fatal(r.Run(":8080"))
-	}()
-
-	for {
-		// Fetch session count
-		count, err := getSessions()
-		if err != nil {
-			log.Printf("Error getting sessions count: %s", err)
-		} else {
-			// Update Prometheus metric with the new session count
-			activeSessions.Set(float64(count))
-		}
-
-		// Sleep for 30 seconds before the next iteration
-		time.Sleep(pollingInterval)
-	}
+	log.Fatal(r.Run(":8080"))
 }
