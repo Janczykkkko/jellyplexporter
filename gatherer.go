07535074d8c25d18aeebfc044060e473b1d80223
Janczykkkko <119225868+Janczykkkko@users.noreply.github.com>
2023-12-06 13:44:51 +0100

    Rewrite detailed sessions (#2)
    Rewritten the exporter.
    
    Jellyfin apikey var changed.

---
+++ gatherer.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// GetSessions fetches sessions from Jellyfin
+func GetSessions() {
+	var (
+		JellyJSON []JellySession
+		count     int
+	)
+	url := jellyfinAddress + "/Sessions?api_key=" + jellyfinApiKey
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching sessions: " + err.Error())
+	}
+	defer resp.Body.Close()
+	log.Printf("API request to %s completed with status code: %d", jellyfinAddress, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error fetching sessions: " + err.Error())
+	}
+	err = json.Unmarshal(body, &JellyJSON)
+	if err != nil {
+		fmt.Println("Error fetching sessions: " + err.Error())
+	}
+	count = 0
+	prometheus.Unregister(sessionsMetric)
+	prometheus.MustRegister(sessionsMetric)
+	for _, obj := range JellyJSON {
+		if len(obj.NowPlayingQueueFullItems) > 0 &&
+			obj.PlayState.PlayMethod != "" {
+			var userName string
+			var name string
+			var state string
+			var bitrate string
+			var substream string
+			var playMethod string
+			var deviceName string
+			if obj.PlayState.IsPaused {
+				state = "paused"
+			} else {
+				state = "in progress"
+			}
+			if err == nil {
+				bitrateFloat := float64(obj.NowPlayingQueueFullItems[0].MediaSources[0].Bitrate) / 1000000.0
+				bitrate = strconv.FormatFloat(bitrateFloat, 'f', -1, 64)
+			} else {
+				bitrate = "error"
+			}
+			name = obj.NowPlayingQueueFullItems[0].MediaSources[0].Name
+			userName = obj.UserName
+			playMethod = obj.PlayState.PlayMethod
+			deviceName = obj.DeviceName
+			SubtitleStreamIndex := obj.PlayState.SubtitleStreamIndex
+			if SubtitleStreamIndex >= 0 && SubtitleStreamIndex < len(obj.NowPlayingQueueFullItems[0].MediaStreams) {
+				substream = obj.NowPlayingQueueFullItems[0].MediaStreams[obj.PlayState.SubtitleStreamIndex].DisplayTitle
+			} else {
+				substream = "None"
+			}
+			count++
+			updateSessionMetrics(userName, state, name, playMethod, substream, deviceName, bitrate, count)
+		} else {
+			continue
+		}
+
+	}
+	if count == 0 {
+		// No sessions found, reset metric to zero
+		prometheus.Unregister(sessionsMetric)
+	}
+
+}
+
+func updateSessionMetrics(username, state, name, playMethod, substream, deviceName string, bitrate string, count int) {
+	sessionLabels := prometheus.Labels{
+		"UserName":   username,
+		"State":      state,
+		"Name":       name,
+		"Bitrate":    bitrate,
+		"PlayMethod": playMethod,
+		"Substream":  substream,
+		"DeviceName": deviceName,
+	}
+
+	// Set labels and update the gauge for the specific session
+	sessionsMetric.With(sessionLabels).Set(float64(count))
+}
--- main.go
+++ main.go
@@ -1,77 +1,49 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/PaesslerAG/jsonpath"
-	"github.com/gin-gonic/gin"
-)
-
-var (
-	pollingInterval = 30 * time.Second // Default polling interval
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
 	jellyfinAddress string
-	apiKey          string
+	jellyfinApiKey  string
+	sessionsMetric  = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "jellyfin_session_info",
+			Help: "Information about Jellyfin sessions",
+		},
+		[]string{"UserName", "State", "Name", "Bitrate", "PlayMethod", "Substream", "DeviceName"},
+	)
 )
-
-func getSessions() (int, error) {
-	url := jellyfinAddress + "/Sessions?api_key=" + apiKey
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	log.Printf("API request to %s completed with status code: %d", jellyfinAddress, resp.StatusCode)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-	var jsonData interface{}
-	err = json.Unmarshal(body, &jsonData)
-	if err != nil {
-		return 0, err
-	}
-	result, err := jsonpath.Get("$[*].NowPlayingItem", jsonData)
-	if err != nil {
-		return 0, err
-	}
-	count := len(result.([]interface{}))
-	return count, nil
-}
 
 func main() {
 	jellyfinAddress = os.Getenv("JELLYFIN_ADDRESS")
-	apiKey = os.Getenv("API_KEY")
-	pollingIntervalStr := os.Getenv("POLLING_INTERVAL")
-	if pollingIntervalStr != "" {
-		interval, err := strconv.Atoi(pollingIntervalStr)
-		if err != nil {
-			log.Fatalf("Invalid value for POLLING_INTERVAL: %s", err)
-		}
-		pollingInterval = time.Duration(interval) * time.Second
-	}
+	jellyfinApiKey = os.Getenv("JELLYFIN_APIKEY")
 
-	if jellyfinAddress == "" || apiKey == "" {
+	if jellyfinAddress == "" || jellyfinApiKey == "" {
 		log.Fatal("Please provide Jellyfin address and API key")
 	}
 
-	r := gin.Default()
+	http.Handle("/metrics", promhttp.Handler())
 
-	r.GET("/metrics", func(c *gin.Context) {
-		count, err := getSessions()
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error getting sessions count")
-			return
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	interval := 30 * time.Second
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	GetSessions()
+	for {
+		select {
+		case <-ticker.C:
+			GetSessions()
 		}
-		c.String(http.StatusOK, "jellyfin_active_sessions_count %d", count)
-	})
-	log.Fatal(r.Run(":8080"))
+	}
 }
