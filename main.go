cd7553035bcca71f5dc5dfec2138c7ebb297f54a
Jan Romaniszyn <jan.romaniszyn@gmail.com>
2023-12-13 15:35:30 +0100

    refactor gatherers, add plex metrics

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"jellyplexporter/gatherers"
 	"log"
 	"net/http"
 	"os"
@@ -20,30 +21,94 @@ var (
 		},
 		[]string{"UserName", "Name", "Bitrate", "PlayMethod", "Substream", "DeviceName"},
 	)
+	plexAddress       string
+	plexApiKey        string
+	PlexSessionMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "plex_session_info",
+			Help: "Information about Plex sessions",
+		},
+		[]string{"UserName", "Name", "Bitrate", "PlayMethod", "Substream", "DeviceName"},
+	)
 )
 
 func main() {
 	jellyfinAddress = os.Getenv("JELLYFIN_ADDRESS")
 	jellyfinApiKey = os.Getenv("JELLYFIN_APIKEY")
-
-	if jellyfinAddress == "" || jellyfinApiKey == "" {
-		log.Fatal("Please provide Jellyfin address and API key")
-	}
+	plexAddress = os.Getenv("PLEX_ADDRESS")
+	plexApiKey = os.Getenv("PLEX_APIKEY")
+	enableJellyfin, enablePlex := checkEnvs()
 
 	http.Handle("/metrics", promhttp.Handler())
 
 	go func() {
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	}()
-	prometheus.MustRegister(JellyfinSessionsMetric)
-	interval := 30 * time.Second
-	ticker := time.NewTicker(interval)
+
+	if enableJellyfin {
+		prometheus.MustRegister(JellyfinSessionsMetric)
+	}
+	if enablePlex {
+		prometheus.MustRegister(PlexSessionMetric)
+	}
+
+	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
-	GetJellyfinSessions()
-	for {
-		select {
-		case <-ticker.C:
-			GetJellyfinSessions()
+
+	for range ticker.C {
+		updateMetrics(enableJellyfin, enablePlex)
+	}
+}
+
+func updateMetrics(enableJellyfin, enablePlex bool) {
+	if enableJellyfin {
+		JellyfinSessionsMetric.Reset() // reset to remove any bugged metrics
+		metrics, err := gatherers.GetJellyMetrics(jellyfinAddress, jellyfinApiKey)
+		if err != nil {
+			log.Printf("Error getting Jellyfin session info: %s", err)
+		}
+		for _, metric := range metrics {
+			sessionLabels := prometheus.Labels{
+				"UserName":   metric.UserName,
+				"Name":       metric.Name,
+				"Bitrate":    metric.Bitrate,
+				"PlayMethod": metric.PlayMethod,
+				"Substream":  metric.SubStream,
+				"DeviceName": metric.DeviceName,
+			}
+			JellyfinSessionsMetric.With(sessionLabels).Set(float64(1))
 		}
 	}
+	if enablePlex {
+		PlexSessionMetric.Reset() // reset to remove any bugged metrics
+		metrics, err := gatherers.GetPlexMetrics(plexAddress, plexApiKey)
+		if err != nil {
+			log.Printf("Error getting Plex session info: %s", err)
+		}
+		for _, metric := range metrics {
+			sessionLabels := prometheus.Labels{
+				"UserName":   metric.UserName,
+				"Name":       metric.Name,
+				"Bitrate":    metric.Bitrate,
+				"PlayMethod": metric.PlayMethod,
+				"Substream":  metric.SubStream,
+				"DeviceName": metric.DeviceName,
+			}
+			PlexSessionMetric.With(sessionLabels).Set(float64(1))
+		}
+	}
+}
+
+func checkEnvs() (enableJellyfin, enablePlex bool) {
+	enableJellyfin = true
+	enablePlex = true
+	if jellyfinAddress == "" || jellyfinApiKey == "" {
+		log.Println("Jellyfin address and API key, Jellyfin metrics will not be exported")
+		enableJellyfin = false
+	}
+	if plexAddress == "" || plexApiKey == "" {
+		log.Println("Plex address and API key, Plex metrics will not be exported")
+		enablePlex = false
+	}
+	return enableJellyfin, enablePlex
 }
